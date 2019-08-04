4ddd9e8219cec3fa6f216006c7949882b919a5f9
Avner <Hattabavner@gmail.com>
2019-08-04 12:29:03 +0300

    pagination added

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -1,13 +1,6 @@
 import { Product } from './../models/product';
 import { Component } from '@angular/core';
 
-export enum PageName {
-  Home = 'Home',
-  About = 'About',
-  Products = 'Products',
-  Contact = 'Contact'
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,7 +8,7 @@ export enum PageName {
 })
 export class AppComponent {
   title = 'shop';
-  currentPage: PageName = PageName.Home;
+  currentPage: string = 'Home';
   menubarMode: boolean = false;
   productList: Product[] = [
     new Product('1234', 'assets/products/11.jpeg', 'dog', 1000, 'Just a dog'),
@@ -25,12 +18,13 @@ export class AppComponent {
     new Product('1234', 'assets/products/11.jpeg', 'dog', 1200, 'Just a dog'),
     new Product('1234', 'assets/products/11.jpeg', 'dog', 1200, 'Just a dog')
   ];
+
   pagesNames = ['Home', 'About', 'Products', 'Contact'];
 
   toggleMenuBar() {
     this.menubarMode = !this.menubarMode;
   }
-  changePage(page: PageName) {
-    console.log(page);
+  changePage(page: string) {
+    this.currentPage = page;
   }
 }
