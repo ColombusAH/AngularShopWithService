9d416b781eceebc0cbc1765e4ad1a1a767d53c61
ColombusAH <Hattabavner@gmail.com>
2019-08-28 11:07:32 +0300

    remove unacessary code

--- shop/src/app/components/ngbd-modal-confirm/ngbd-modal-confirm.component.ts
+++ shop/src/app/components/ngbd-modal-confirm/ngbd-modal-confirm.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-ngbd-modal-confirm',
@@ -7,7 +7,6 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./ngbd-modal-confirm.component.css']
 })
 export class NgbdModalConfirmComponent {
-  closeResult: string;
   @Input() disabled: boolean;
   @Output() okButtonClicked = new EventEmitter();
   constructor(private modalService: NgbModal) {}
@@ -17,26 +16,11 @@ export class NgbdModalConfirmComponent {
       .open(content, { ariaLabelledBy: 'modal-basic-title' })
       .result.then(
         result => {
-          console.log('okokok');
-
           this.okButtonClicked.emit();
-          this.closeResult = `Closed with: ${result}`;
         },
         reason => {
           console.log('xxxxxxx');
-
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
         }
       );
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
   }
 }
