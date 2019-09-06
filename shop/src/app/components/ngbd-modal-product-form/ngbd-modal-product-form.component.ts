b39b2bce6a6040918c59eb890ec881d390392884
ColombusAH <Hattabavner@gmail.com>
2019-09-06 17:45:19 +0300

    cfirmation added

--- shop/src/app/components/ngbd-modal-product-form/ngbd-modal-product-form.component.ts
+++ shop/src/app/components/ngbd-modal-product-form/ngbd-modal-product-form.component.ts
@@ -1,5 +1,5 @@
 import { Product } from './../../models/product.model';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -7,15 +7,25 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './ngbd-modal-product-form.component.html',
   styleUrls: ['./ngbd-modal-product-form.component.css']
 })
-export class NgbdModalProductFormComponent implements OnInit {
+export class NgbdModalProductFormComponent {
   @Input() product: Product;
   constructor(private modalService: NgbModal) {}
-
+  isDirty: boolean = false;
   open(content) {
     this.modalService
       .open(content, { ariaLabelledBy: 'modal-basic-title' })
       .result.then(result => {}, reason => {});
   }
 
-  ngOnInit() {}
+  setDirty(state: boolean) {
+    this.isDirty = state;
+  }
+  canDeactivate() {
+    if (this.isDirty) {
+      const ans = confirm('are you sure?');
+      if (ans) {
+        this.modalService.dismissAll();
+      }
+    }
+  }
 }
