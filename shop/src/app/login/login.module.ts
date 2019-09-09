fda4ca3df1f548e6942083668072a2c897566186
ColombusAH <Hattabavner@gmail.com>
2019-09-09 14:22:02 +0300

    login module added

---
+++ shop/src/app/login/login.module.ts
@@ -0,0 +1,12 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginPageComponent } from './login-page.component';
+
+@NgModule({
+  declarations: [LoginPageComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, LoginRoutingModule]
+})
+export class LoginModule {}
