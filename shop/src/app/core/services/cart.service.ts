39c3bf6544b4d9c5936bf228c894ec7bad5da3c2
ColombusAH <Hattabavner@gmail.com>
2019-09-13 10:19:24 +0300

    bug fixed

--- shop/src/app/core/services/cart.service.ts
+++ shop/src/app/core/services/cart.service.ts
@@ -17,104 +17,102 @@ export interface ICartState {
 export class CartService {
   private _shoppingListSubject: BehaviorSubject<ICartState>;
   public CartState$: Observable<ICartState>;
+
   private cart: ICartState = {
     productsList: [],
     size: 0,
     totalPrice: 0
   };
 
-  private _productItems: ProductItem[] = [];
-  private _size: number = 0;
-  private _totalPrice: number = 0;
-
   constructor(private userService: UserService) {
     this.initializeCartState();
+
+    this._shoppingListSubject = new BehaviorSubject({
+      productsList: this.cart.productsList,
+      size: this.getTotalSize(),
+      totalPrice: this.getTotalPrice()
+    });
     this.CartState$ = this._shoppingListSubject.asObservable();
 
     this.userService.currentUserState.subscribe(user => {
       if (!user) {
-        this.initializeCartState();
+        this.emptyCart();
       }
     });
   }
 
   addProduct(product: Product): void {
-    this._totalPrice += product.price;
-    this._size += 1;
+    this.cart.totalPrice += product.price;
+    this.cart.size += 1;
     const index = _.findIndex(
-      this._productItems,
+      this.cart.productsList,
       pi => pi.product.id === product.id
     );
     if (index === -1) {
-      this._productItems.push(new ProductItem(product));
+      this.cart.productsList.push(new ProductItem(product));
     } else {
-      this._productItems[index].quantity += 1;
+      this.cart.productsList[index].quantity += 1;
     }
     this.setNext();
   }
 
   removeProduct(product: Product): void {
     const index = _.findIndex(
-      this._productItems,
+      this.cart.productsList,
       pi => pi.product.id === product.id
     );
     if (index !== -1) {
-      this._totalPrice -= product.price;
-      this._size -= 1;
-      this._productItems[index].quantity -= 1;
-      if (this._productItems[index].quantity === 0) {
-        _.remove(this._productItems, pi => pi.product.id === product.id);
+      this.cart.totalPrice -= product.price;
+      this.cart.size -= 1;
+      this.cart.productsList[index].quantity -= 1;
+      if (this.cart.productsList[index].quantity === 0) {
+        _.remove(this.cart.productsList, pi => pi.product.id === product.id);
       }
       this.setNext();
     }
   }
 
   private setNext() {
-    this._shoppingListSubject.next({
-      productsList: this._productItems,
-      size: this._size,
-      totalPrice: this._totalPrice
-    });
-    localStorage.setItem('cart', JSON.stringify(this._productItems));
+    this._shoppingListSubject.next(this.cart);
+    localStorage.setItem('cart', JSON.stringify(this.cart.productsList));
   }
 
   emptyCart(): void {
-    this._productItems = [] as ProductItem[];
-    this._size = 0;
-    this._totalPrice = 0;
+    this.cart.productsList = [];
+    this.cart.size = 0;
+    this.cart.totalPrice = 0;
     this.setNext();
   }
 
   getQuantityInShopingList(prodduct: Product) {
-    const index = this._productItems.findIndex(
+    const index = this.cart.productsList.findIndex(
       pi => prodduct.id === pi.product.id
     );
     if (index === -1) {
       return 0;
     } else {
-      return this._productItems[index].quantity;
+      return this.cart.productsList[index].quantity;
     }
   }
 
   getTotalSize(): number {
     let size = 0;
-    this._productItems.forEach(pi => (size += pi.quantity));
+    this.cart.productsList.forEach(pi => (size += pi.quantity));
     return size;
   }
 
   getTotalPrice(): number {
     let price = 0;
-    this._productItems.forEach(pi => (price += pi.quantity * pi.product.price));
+    this.cart.productsList.forEach(
+      pi => (price += pi.quantity * pi.product.price)
+    );
     return price;
   }
 
   initializeCartState(): void {
-    this._productItems =
+    this.cart.productsList =
       (JSON.parse(localStorage.getItem('cart')) as ProductItem[]) || [];
-    this._shoppingListSubject = new BehaviorSubject({
-      productsList: this._productItems,
-      size: this.getTotalSize(),
-      totalPrice: this.getTotalPrice()
-    });
+    this.cart.totalPrice = this.getTotalPrice();
+    this.cart.size = this.getTotalSize();
   }
 }
